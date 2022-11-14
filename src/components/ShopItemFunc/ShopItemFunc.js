6bcad3db0929f050d92d230297f3b13d418830a5
gotan-dancer <eugene.kalugin@gmail.com>
2022-11-14 15:02:22 +0500

    Initial commit

---
+++ src/components/ShopItemFunc/ShopItemFunc.js
@@ -0,0 +1,21 @@
+function ShopItemFunc(props)
+{
+    const {item} = props;
+    
+    return (
+        <div>
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div class="description">{item.descriptionFull}</div>
+            <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+            <div class="divider"></div>
+                <div class="purchase-info">
+                <div class="price">{item.currency}{(item.price).toString()+".00"}</div>
+                    <button>Добавить в корзину</button>
+                </div>            
+        </div>
+    );
+}
+
+export default ShopItemFunc;
